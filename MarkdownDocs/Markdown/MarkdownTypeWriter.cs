07533c38e4f4f0bae2d302c038f997a3b7f73670
Emanuel Miroiu <miroiu.emanuel@yahoo.com>
2021-06-15 22:32:27 +0300

    Write more data about types

--- MarkdownDocs/Markdown/MarkdownTypeWriter.cs
+++ MarkdownDocs/Markdown/MarkdownTypeWriter.cs
@@ -1,5 +1,7 @@
 ﻿using MarkdownDocs.Metadata;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -26,22 +28,51 @@ namespace MarkdownDocs.Markdown
             _writer.WriteLine($"{"Namespace:".Bold()} {type.Namespace}");
             _writer.WriteLine($"{"Assembly:".Bold()} {type.Assembly}");
 
-            if (type.Category == TypeCategory.Class)
+            _writer.WriteLine($"{"Inheritance:".Bold()} {GetInheritanceChain(type)}");
+
+            WriteImplementations(type);
+            WriteDerivedTypes(type);
+            WriteReferences(type);
+        }
+
+        private void WriteReferences(ITypeMetadata type)
+        {
+            string references = string.Join(", ", type.References.Select(i => i.Link(type, _options)));
+            if (references.Length > 0)
             {
-                _writer.WriteLine($"{"Inheritance:".Bold()} {GetInheritanceChain(type)}");
+                _writer.WriteLine($"{"References:".Bold()} {references}");
+            }
+        }
+
+        private void WriteDerivedTypes(ITypeMetadata type)
+        {
+            string derived = string.Join(", ", type.Derived.Select(i => i.Link(type, _options)));
+            if (derived.Length > 0)
+            {
+                _writer.WriteLine($"{"Derived:".Bold()} {derived}");
+            }
+        }
+
+        private void WriteImplementations(ITypeMetadata type)
+        {
+            string implementations = string.Join(", ", type.Implemented.Select(i => i.Link(type, _options)));
+            if (implementations.Length > 0)
+            {
+                _writer.WriteLine($"{"Implements:".Bold()} {implementations}");
             }
         }
 
         private string GetInheritanceChain(ITypeMetadata type)
         {
             var baseTypes = new List<string>();
-            ITypeMetadata? current = type.Inherited;
+            ITypeMetadata? current = type;
 
-            while (current != null)
+            do
             {
                 baseTypes.Add(current.Link(type, _options));
                 current = current.Inherited;
             }
+            while (current != null);
 
             baseTypes.Reverse();
             string result = string.Join(" → ", baseTypes);
